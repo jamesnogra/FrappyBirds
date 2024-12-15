6b58fa15783aa84b4a7c15d1b7e1337aa27c960e
jamesnogra <jamesnogra@gmail.com>
2024-12-15 10:45:15 +0800

    Implement random height of pipes.

--- Assets/PipeSpawnScript.cs
+++ Assets/PipeSpawnScript.cs
@@ -5,6 +5,7 @@ public class PipeSpawnScript : MonoBehaviour
     public GameObject pipe;
     public float spawnRate = 4;
     private float timer = 0;
+    public float heightOffset = 4;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -28,6 +29,12 @@ public class PipeSpawnScript : MonoBehaviour
 
     private void spawnPipe()
     {
-        Instantiate(pipe, transform.position, transform.rotation);
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+        Instantiate(
+            pipe,
+            new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0),
+            transform.rotation
+        );
     }
 }
